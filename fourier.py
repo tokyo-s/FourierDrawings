b54b5a5ddfc99660db67cae570773a55ea50328d
vladimir stojoc <vladimir.stojoc@gmail.com>
2021-03-07 20:15:25 +0200

    first commit

---
+++ fourier.py
@@ -0,0 +1,170 @@
+import cmath
+import sys
+from flask import Blueprint, render_template, request, jsonify, current_app
+from scipy import integrate
+import numpy as np
+import matplotlib.pyplot as plt
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.fields import SubmitField, IntegerField
+from svgpathtools import svg2paths
+from svg2paths_ext import svg_file2paths
+from xml.dom.minidom import parse
+import svgutils
+import os
+from werkzeug.utils import secure_filename
+from svgpathtools import smoothed_path
+
+fourier_blueprint = Blueprint('fourier', __name__)
+
+class FourierForm(FlaskForm):
+    svg_file = FileField(u"SVG to plot", validators=[
+        FileRequired(),
+        FileAllowed(['svg'], 'svgs only!')
+    ])
+    canvas_width = IntegerField(u"The canvas width to plot on")
+    canvas_height = IntegerField(u"The canvas height to plot on")
+    number_of_circles = IntegerField(u"How many circles to draw")
+    submit = SubmitField("Plot")
+
+
+@fourier_blueprint.route("/")
+def fourier_index():
+    print('fdfsdfds')
+    return render_template('fourier.html', form=FourierForm())
+
+
+@fourier_blueprint.route('/fourier/get_constants', methods=['PUT', 'POST'])
+def get_fourier_constants_post():
+    form = FourierForm()
+    if form.validate_on_submit():
+        f = form.svg_file.data
+        file = request.files['file']
+        print(file)
+        # f.save(os.getcwd())
+        # svg = svgutils.transform.fromfile(f.filename)
+        #f.rotate()
+        #f.save("D:\\programming\\ML\\graphimages\\FourierFromSVG-master\\FourierFromSVG-master")
+        #doc = parse(f)
+        number_of_circles = form.number_of_circles.data
+        canvas_width = form.canvas_width.data
+        canvas_height = form.canvas_height.data
+        current_app.logger.debug('Calculating fourier constants for %s with %d circles', f.filename, number_of_circles)
+        fourier_constants = calculate_fourier_for_svg(form,f, number_of_circles, canvas_width, canvas_height)
+        return jsonify(convert_fourier_constants_for_js(fourier_constants))
+    return jsonify(data=form.errors)
+
+
+def convert_fourier_constants_for_js(constants):
+    result = []
+    for constant in constants:
+        result.append(convert_fourier_constant_for_js(constant))
+    return result
+
+
+def convert_fourier_constant_for_js(constant):
+    radius, angle = cmath.polar(constant['C'])
+    return {
+        's': constant['speed'],
+        'r': radius,
+        'a': angle
+    }
+
+
+def calculate_fourier_for_svg_path(svg_path, count):
+    paths, _ = svg2paths(svg_path)
+    return calculate_fourier_for_path(paths[count], count, 600, 600)
+
+
+def calculate_fourier_for_svg(form,svg_file, count, canvas_width, canvas_height):
+    paths, attributes = svg_file2paths(svg_file)
+    return calculate_fourier_for_path(form,paths, count, canvas_width, canvas_height)
+
+
+def complex_integrate(func, *args):
+    result = integrate.fixed_quad(func, 0, 1, n=500, args=args)[0]
+    return result
+
+
+def scale_svg(path, x, y):
+    orig_bbox = path.bbox()
+    path = path.translated(-(orig_bbox[0] - 20) - (orig_bbox[2] - 20) * 1j)
+    y_diff = orig_bbox[3] - orig_bbox[2]
+    x_diff = orig_bbox[1] - orig_bbox[0]
+    scale_factor = min(x / x_diff, y / y_diff)
+    scaled = path.scaled(scale_factor, scale_factor)
+    return scaled
+
+
+def calculate_fourier_for_path(form,paths, count, canvas_width, canvas_height):
+    result = []
+    maxx= -999999999
+    maxy = -99999999
+    minx = 99999999
+    miny = 99999999
+    for path in paths:
+        #path = path.rotated(180)
+        maxx = max(maxx,path.bbox()[1])
+        minx = min(minx,path.bbox()[0])
+        miny = min(miny,path.bbox()[2])
+        maxy = max(maxy,path.bbox()[3])
+
+    print("x - ",minx,' ',maxx)
+    print("y - ",miny,' ',maxy)
+    # form.canvas_width.data = (maxx-minx)/10
+    # form.canvas_height.data = (maxy-miny)/10
+    canvas_width = (maxx-minx)/10
+    canvas_height = (maxy-miny)/10
+    for i,path in enumerate(paths):
+        #path = path.rotated(180)
+        #print(path.getBoundig)
+        #print(path.getBoundingClientRect().width)
+        xmin, xmax, ymin, ymax = path.bbox()
+        
+        height = ymax - ymin
+        width = xmax - xmin
+        print(i)
+        print("xmin = ",xmin)
+        print("ymin = ",ymin)
+        widthpercent=width/(maxx-minx)
+        heightpercent = height/(maxy-miny)
+        path = scale_svg(path,widthpercent*canvas_width,heightpercent*canvas_height)
+        
+        #path = path.translated()#+xmin/(maxx-minx)*canvas_width+ymin/(maxy-miny)*canvas_height*1j
+        
+
+
+        
+        #path = path.translated(-(xmin - 20) - (ymin - 20) * 1j)
+        _get_path_point = np.vectorize(lambda x: path.point(x))
+        def _func(x, n):
+            return np.exp(-n * 2 * np.pi * x * 1j) * _get_path_point(x)
+
+        result.append({'speed': 0, 'C': complex_integrate(_func, 0)+(xmin)/(maxx-minx)*canvas_width+(ymin)/(maxy-miny)*canvas_height*1j+1500*1j})#+xmin/maxx*canvas_width+ymin/(maxy/canvas_height)*1j
+        for i in range(1, count):
+            result.append({'speed': i, 'C': complex_integrate(_func, i)})
+            result.append({'speed': -i, 'C': complex_integrate(_func, -i)})
+    return result
+
+
+# def plot_svg_path(svg):
+#     paths, _ = svg2paths(svg)
+
+#     def _func(x):
+#         return paths[0].point(x)
+
+#     vct = np.vectorize(_func)
+#     line = np.linspace(0, 1, num=5000)
+#     cnums = vct(line)
+#     X = [x.real for x in cnums]
+#     Y = [x.imag for x in cnums]
+#     # plt.scatter(X,Y, color='black')
+#     plt.plot(line, X)
+#     plt.plot(line, Y)
+#     plt.show()
+
+
+if __name__ == "__main__":
+    # plot_svg_path(sys.argv[1])
+    print(convert_fourier_constants_for_js(
+        calculate_fourier_for_svg_path(sys.argv[1], int(sys.argv[2]))))
---
+++ static/js/fourier.js
@@ -0,0 +1,175 @@
+
+let canvas = null; 
+let context = null;
+let time = 0;
+const Point = class {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    equals(point) {
+        return this.x === point.x && this.y === point.y;
+    }
+    toString() {
+        return '(' + this.x + ', ' + this.y + ')';
+    }
+
+};
+
+const FourierCircle = class {
+    constructor(speed, radius, initial_angle)
+    {
+        this.radius = radius/2;
+        this.speed = speed/20;
+        this.initial_angle = initial_angle
+    }
+    draw(ctx, at) 
+    {
+        ctx.beginPath();
+        //ctx.arc(at.x, at.y, this.radius, 0, Math.PI * 2, true);
+        var x = at.x + this.radius * Math.cos(this.initial_angle + 2 * Math.PI * time * this.speed);
+        var y = at.y + this.radius * Math.sin(this.initial_angle + 2 * Math.PI * time * this.speed);
+        ctx.moveTo(at.x, at.y);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.strokeStyle = 'rgba(202, 126, 86, 0.7)';
+        ctx.lineWidth = 1; 
+        ctx.stroke();
+    }
+
+    nextCenter(at) 
+    {
+        var x = at.x + this.radius * Math.cos(this.initial_angle + 2 * Math.PI * time * this.speed);
+        var y = at.y + this.radius * Math.sin(this.initial_angle + 2 * Math.PI * time * this.speed);
+        return new Point(x, y)
+    }
+};
+
+let n;
+let circles;
+let animation_id = 0;
+let center = new Point(150, 150);
+let wave = [];
+let nr_of_paths;
+let flag = false;
+function init_fourier(canvas_elm, constants, count) {
+    canvas = canvas_elm;
+    context = canvas.getContext('2d');
+    if(flag==false){
+        context.transform(1, 0, 0, -1, 0, canvas.height);
+        flag=true;
+    }
+
+    if(animation_id !== 0)
+        window.cancelAnimationFrame(animation_id);
+    n = count;
+
+    circles = [];
+    nr_of_paths = constants.length/((count-1)*2+1);
+    for(let i=0;i<nr_of_paths;i++){
+        wave.push([]);
+    } 
+    console.log("lenght const"+ constants.length)
+
+    for (let i = 0; i < constants.length; i++) {
+        let constant = constants[i];
+        circles[i] = new FourierCircle(constant.s, constant.r, constant.a);
+    }
+    animation_id = window.requestAnimationFrame(draw);
+}
+
+function draw_wave(ctx,wave,j) {
+    // ctx.beginPath();
+    for (let i = 1; i < wave[j].length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(wave[j][i-1].x, wave[j][i-1].y);
+        ctx.lineTo(wave[j][i].x, wave[j][i].y);
+        ctx.closePath();
+
+        // let c = Math.ceil(127.0 + 128.0*i/wave.length);
+        let alpha = 1 - i*1.0/wave[j].length;
+        
+        ctx.strokeStyle = 'rgba(0, 0, 0, ' + alpha + ')';
+        //ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+        ctx.lineWidth = 1;
+        ctx.stroke();
+    }
+    // ctx.closePath();
+    // ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+    // ctx.stroke();
+} 
+
+// function draw_wave2(ctx) {
+//     // ctx.beginPath();
+//     for (let i = 1; i < wave2.length; i++) {
+//         ctx.beginPath();
+//         ctx.moveTo(wave2[i-1].x, wave2[i-1].y);
+//         ctx.lineTo(wave2[i].x, wave2[i].y);
+//         ctx.closePath();
+
+//         // let c = Math.ceil(127.0 + 128.0*i/wave.length);
+//         let alpha = 1 - i*1.0/wave2.length;
+        
+//         ctx.strokeStyle = 'rgba(0, 0, 0, ' + alpha + ')';
+//         //ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+//         ctx.lineWidth = 1;
+//         ctx.stroke();
+//     }
+//     // ctx.closePath();
+//     // ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+//     // ctx.stroke();
+// } 
+
+
+function draw() {
+    console.log("ia tut ne bil");
+    context.clearRect(0,0, canvas.width, canvas.height);
+    // let new_center = center;
+    let count;
+
+
+    for(let j=0;j<nr_of_paths;j++){
+        count = 2*n*j-1*j;
+        if(j==0)count=0;
+        console.log("count for "+j+" count = "+count)
+        console.log(nr_of_paths)
+        var new_center = circles[count].nextCenter(center);
+        for(let i = 1; i < n; i++) {
+            circles[i+count].draw(context, new_center);
+         new_center = circles[i+count].nextCenter(new_center);
+        }
+          draw_wave(context,wave,j);
+          wave[j].unshift(new_center);
+
+          if(wave[j].length > 500) {
+            wave[j].pop();
+        }
+    }
+
+
+
+
+    
+
+
+    // var new_center = circles[201].nextCenter(center);
+    // for(let i = 1; i < n; i++) {
+    //     circles[i+201].draw(context, new_center);
+    //     new_center = circles[i+201].nextCenter(new_center);
+    // }
+    //   draw_wave(context,wave2);
+    //   wave2.unshift(new_center);
+
+
+    //   console.log(wave);
+    //   console.log(wave2.length);
+        
+        
+    
+    
+
+    animation_id = window.requestAnimationFrame(draw);
+
+    time += 0.04;
+    
+}
